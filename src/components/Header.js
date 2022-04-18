230996f2c91ddde846a715cb59214690a53f16b0
ryurim0109 <dbfla5036@naver.com>
2022-04-18 13:18:22 +0900

    헤더 무늬만 완성

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,8 @@ import {history} from "../redux/configStore";
 import styled from 'styled-components';
 import defaultUserImage from '../img/logo.png';
 import {AiFillHome} from 'react-icons/ai';
+import { CgMenuGridO } from "react-icons/cg";
+
 
 const Header =(props)=>{
     return (
@@ -38,7 +40,10 @@ const Header =(props)=>{
                 </svg>
               </div>
             </Middle>
-            <MainGrid display="flex"alignItems="center" width="176px" padding="0 36px 0 0">
+            <MainGrid display="flex"alignItems="center" width="240px" padding="0 36px 0 0">
+            <Btn>
+              <CgMenuGridO  style={{fontSize:"28px"}}/>
+              </Btn>
               <Btn>
               <svg style={{ height: '26', width: '26' }}>
                 <path d='M14 2.042c6.76 0 12 4.952 12 11.64S20.76 25.322 14 25.322a13.091 13.091 0 0 1-3.474-.461.956 .956 0 0 0-.641.047L7.5 25.959a.961.961 0 0 1-1.348-.849l-.065-2.134a.957.957 0 0 0-.322-.684A11.389 11.389 0 0 1 2 13.682C2 6.994 7.24 2.042 14 2.042ZM6.794 17.086a.57.57 0 0 0 .827.758l3.786-2.874a.722.722 0 0 1 .868 0l2.8 2.1a1.8 1.8 0 0 0 2.6-.481l3.525-5.592a.57.57 0 0 0-.827-.758l-3.786 2.874a.722.722 0 0 1-.868 0l-2.8-2.1a1.8 1.8 0 0 0-2.6.481Z'></path>
