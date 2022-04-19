e326fb69f14e36ddcab7c4fd0c5293cc21aff147
ryurim0109 <dbfla5036@naver.com>
2022-04-19 10:37:29 +0900

    연락처 만드는중

---
+++ src/components/SideBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { MainGrid } from '../elements';
+
+const SideBar =(props)=>{
+
+    return(
+        <>
+            <MainGrid>
+                연락처
+            </MainGrid>
+        </>
+    )
+};
+
+export default SideBar;
